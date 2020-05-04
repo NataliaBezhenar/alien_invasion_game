e1478547a549a03e0803a4beb9405814b388eb08
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2020-05-04 14:45:31 +0300

    some PEP 8 approvements

--- game_stats.py
+++ game_stats.py
@@ -6,7 +6,6 @@ class GameStats:
         self.reset_stats()
         self.game_active = False
 
-
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
